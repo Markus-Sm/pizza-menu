1e498e60d4f947452edbbf9fe8416d4e921556ab
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-03 19:51:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,6 +52,14 @@ const pizzaData = [
     photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
   },
+
+      {
+    name: "Pizza Mozzarela",
+    ingredients: "Tomato, mozarella",
+    price: 14,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
 ];
 
 console.log(pizzaData)
